79e09e03b2392b1ddb74faa074db6a94a02be3cf
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-09-29 14:15:25 +0200

    Update form & security headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,13 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.trvepeak.studio",
+  server: {
+    headers: {
+      "Content-Security-Policy": "script-src 'self'",
+      "X-Frame-Options": "DENY",
+      "X-Content-Type-Options": "nosniff",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "Permissions-Policy": "geolocation=(), camera=(), microphone=()",
+    },
+  },
 });
