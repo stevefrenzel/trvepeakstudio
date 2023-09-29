cb275ec03ecb8d4d6d53d4dca77862809de14778
Steve Frenzel <43616173+stevefrenzel@users.noreply.github.com>
2023-09-29 15:51:59 +0200

    Add vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,6 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
   site: "https://www.trvepeak.studio",
-  server: {
-    headers: {
-      "Content-Security-Policy": "script-src 'self'",
-      "X-Frame-Options": "DENY",
-      "X-Content-Type-Options": "nosniff",
-      "Referrer-Policy": "strict-origin-when-cross-origin",
-      "Permissions-Policy": "geolocation=(), camera=(), microphone=()",
-    },
-  },
 });
